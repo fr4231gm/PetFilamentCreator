01a9885140b6cdc38de7e520bd27a97825c2eec2
fr4231gm <fr4231gm@gmail.com>
2024-11-09 20:37:29 +0100

    Se empieza con la creación de la GUI

--- Software/PetFilamentCreator/PetFilamentCreator.ino
+++ Software/PetFilamentCreator/PetFilamentCreator.ino
@@ -5,18 +5,35 @@
 const int DIR_PIN = 2;
 const int STEP_PIN = 3;
 
+//Datos para el medidor de temperatura
+const int thermistorPin = A0;  // Pin analógico donde está conectado el termistor
+const float nominalResistance = 100000;  // Resistencia nominal del termistor a 25°C (100 kΩ)
+const float nominalTemperature = 25.0;  // Temperatura nominal del termistor (25°C)
+const float bCoefficient = 3950;  // Coeficiente Beta del termistor
+const float seriesResistorValue = 6800;  // Resistencia en serie (10 kΩ)
+
 //Botones de control
-const int botonArriba = 4;
-const int botonAbajo = 5;
-const int botonDerecha = 6;
-const int botonIzquierda = 7;
-const int botonCentro = 8;
+const int botonArriba = 10;
+const int botonAbajo = 11;
+const int botonDerecha = 12;
+const int botonIzquierda = 13;
+const int botonCentro = 2;
 
 //Variable de la lcd
 LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
 
+//Variables de control
+int opcionMenu;
+boolean procesoEnMarcha;
+
 // Crear una instancia de AccelStepper
 AccelStepper stepper(AccelStepper::DRIVER, STEP_PIN, DIR_PIN);
+
+//Variables propias del programa
+float temperaturaObjetivo;
+float temperaturaReal;
+float velocidadPAP;
+float filamentoExtruido;
 
 void setup() {
   // Configurar la velocidad máxima y aceleración
@@ -30,17 +47,43 @@ void setup() {
   lcd.begin(20, 4);
 
   //Se definen los valores por defecto de las variables
-  int opcionMenu = 0;
-  boolean procesoEnMarcha = false;
+  opcionMenu = 0;
+  procesoEnMarcha = false;
+  temperaturaObjetivo = 0;
+  velocidadPAP = 0; //IMPORTANTE, EN RPM
+  filamentoExtruido = 0; //IMPORTANTE, EN METROS
+
 }
 
 void loop() {
-  // Se controla si se accede o no al menú (botonCentro)
+  int adcValue = analogRead(thermistorPin);  // Lee el valor del ADC
+  float resistance = seriesResistorValue / (1023.0 / adcValue - 1);  // Calcula la resistencia del termistor
+  
+  // Calcula la temperatura usando la ecuación de Steinhart-Hart
+  float temperatura;
+  temperatura = resistance / nominalResistance;  // (R/Ro)
+  temperatura = log(temperatura);  // ln(R/Ro)
+  temperatura /= bCoefficient;  // 1/B * ln(R/Ro)
+  temperatura += 1.0 / (nominalTemperature + 273.15);  // + (1/To)
+  temperatura = 1.0 / temperatura;  // Invertir
+  temperatura -= 273.15;  // Convierte de Kelvin a Celsius
+  temperaturaReal = temperatura;
+  
+  //Pintamos la información de la pantalla de inicio
+  lcd.clear();
+  lcd.println("Temperatura: " + temperaturaReal + "º/" + temperaturaObjetivo + "º");
+  lcd.println("Velocidad del PAP: " + velocidadPAP);
+  lcd.println("Filamento extruido: " + filamentoExtruido + " metros");
+  lcd.println("Estado: ")
 
+  // Se controla si se accede o no al menú (botonCentro)
   if (digitalRead(botonCentro) == HIGH) {
     //Se ha pulsado el botón del centro, entramos en la configuración
     //Apareceran diferentes opciones, dependiendo de la elegida se realizará una acción u otra.
-    lcd.print("Seleccione opción:");
+    lcd.println("Volver a pantalla de información");
+    lcd.println("Configurar temperatura objetivo");
+    lcd.println("Configurar velocidad del PAP");
+    lcd.println("Iniciar proceso");
 
     delay(100);
 
